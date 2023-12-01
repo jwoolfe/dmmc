8683ed3ed8e39776f6a4da8b7e38c5123b1d4887
jwoolfe <jwoolfe@nomail.com>
2023-12-01 15:55:24 -0800

    initial hello world for dmmc artists list.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, DMMC Artists List!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});

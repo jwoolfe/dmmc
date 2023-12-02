17b70cd39568a58f30a82c6ef2d1ed646758cd4d
jwoolfe <jwoolfe@nomail.com>
2023-12-01 17:13:48 -0800

    Seeing what happens if an entire js site is inside html

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
-    html = fs.readFileSync('index.html');
-
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+    path = require('path');
 
 var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+    var filePath;
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
+    if (req.url === '/') {
+        // Default to index.html when the root is requested
+        filePath = 'html/index.html';
     } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
+        // Map requested URL to the corresponding file in the "html" folder
+        filePath = 'html' + req.url;
     }
+
+    var fullPath = path.join(__dirname, filePath);
+
+    // Check if the file exists
+    fs.access(fullPath, fs.constants.F_OK, function(err) {
+        if (!err) {
+            // Read the file and serve its content
+            fs.readFile(fullPath, function(err, data) {
+                if (!err) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(data);
+                } else {
+                    // Error reading the file
+                    res.writeHead(500);
+                    res.end('Internal Server Error');
+                }
+            });
+        } else {
+            // File not found
+            res.writeHead(404);
+            res.end('Not Found');
+        }
+    });
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
